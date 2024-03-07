624f9d8e13cb2620979546eea3ddb73ad614b5cd
Maxwell French <maxwell@frenchnet.org>
2024-03-06 20:31:53 -0500

    Add basic tests to RTG.

--- src/rtg.rs
+++ src/rtg.rs
@@ -15,6 +15,7 @@ pub trait RandomTokenGenerator : Display {
 }
 
 /// Handy implementation of RandomTokenGenerator
+/// This simply returns a random item from the provided list.
 pub struct RTG {
     token_list: Vec<String>
 }
@@ -52,5 +53,32 @@ impl Display for RTG {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
         write!(f, "RTG({})", self.token_list.len())
 
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn retrieval_works() {
+        let tokens = vec!["test1", "Test2", "Test3", "Test4"];
+        let tester = RTG::new(tokens.clone());
+
+        println!("RTG: {}", tester);
+
+        for _ in 0..100 {
+            assert!(tokens.contains(&tester.get_token().as_str()));
+        }
+    }
+
+    #[test]
+    fn display_works() {
+        let tokens = vec!["test1", "Test2", "Test3", "Test4"];
+        let tester = RTG::new(tokens);
+
+        let disp = format!("{}", tester);
+
+        assert_eq!(disp, "RTG(4)");
     }
 }

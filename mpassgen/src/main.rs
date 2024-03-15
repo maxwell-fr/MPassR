6a3f7b4024e41f5fffab0098c7bb0d504d6aec6c
Maxwell French <maxwell@frenchnet.org>
2024-03-14 23:19:21 -0400

    Add proof of concept to generator application.

--- mpassgen/src/main.rs
+++ mpassgen/src/main.rs
@@ -1,3 +1,29 @@
+use regex::Regex;
+
+use mpass::specifier::Specifier;
+
 fn main() {
     println!("Hello, world!");
+
+    let hay = "<html>\
+    <head><title>Test</title>\
+    </head>\
+    <body>\
+    <<flarg:wW#$>>
+    <<frobz:#w W #$>>
+    <<ppwd:W w w w ####>>
+    </body></html>";
+
+    let re = Regex::new(r"<<(?<name>[a-zA-Z]+):(?<spec>[a-zA-Z#$ ]+)>>").unwrap();
+    let caps = re.captures_iter(hay);
+
+    for cap in caps {
+        let name = cap.name("name").unwrap().as_str();
+        let spec = cap.name("spec").unwrap().as_str();
+
+        let specifier = Specifier::try_parse(spec).unwrap();
+        println!("{} ({}): {}", name, spec, specifier.get_passphrase());
+
+    }
+
 }
